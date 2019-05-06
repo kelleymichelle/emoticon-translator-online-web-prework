0b1b8ec3bf8fad5c8da57899bcdd74bfc5bf9e07
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-06 03:33:55 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
-# require modules here
+require 'pry'
 
-def load_library
-  # code goes here
+require "yaml"
+x = ('lib/emoticons.yml')
+
+def load_library(x)
+  #binding.pry
+  emoji = YAML.load_file(x)
+  new = {}
+    new["get_emoticon"] = {}
+    new["get_meaning"] = {}
+  
+  emoji.map do |a, b|
+    new["get_emoticon"][b[0]] = b[1]
+    new["get_meaning"][b[1]] = a
+  end
+  return new
 end
 
 def get_japanese_emoticon
